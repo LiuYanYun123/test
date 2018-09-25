e1a78118cedb347855b19286d6d79b2c6ac70158
root <root@localhost.localdomain>
2018-09-25 16:21:47 +0800

    ceshi

--- day01.py
+++ day01.py
@@ -1,2 +1,17 @@
-print("hello world");
+#chang=float(raw_input(">>"))
+#kuan=float(raw_input(">>"))
+#print(chang*kuan)
 
+#s=int(raw_input(">>"))
+#a=s//60
+#b=s%60
+#print(a,b)
+
+
+x=1
+y=1
+a=0
+b=0
+c=0
+
+print(((3+4*x)/5)-((10*(y-5))*(a+b+c))/x+(9*(4/x+(9+x)/y)))
